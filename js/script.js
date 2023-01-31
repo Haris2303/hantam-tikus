f3cedc5d30d022ec46ac89acd0b0e2ffa928bea7
haris2303 <hrsccf102@gmail.com>
2023-01-31 22:31:02 +0900

    initial commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const tanah = document.querySelectorAll('.tanah');
+const tikus = document.querySelectorAll('.tikus');
+const papanSkor = document.querySelector('.papanSkor');
+const bom = document.querySelector('#bom');
+
+let tanahSebelumnya;
+let selesai;
+let skor;
+
+function randomTanah(tanah) {
+  const t = Math.floor(Math.random() * tanah.length);
+  const tRandom = tanah[t];
+
+  if( tRandom === tanahSebelumnya ) {
+    randomTanah(tanah);
+  }
+
+  tanahSebelumnya = tRandom
+
+  return tRandom;
+}
+
+function randomWaktu(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function munculkanTikus() {
+  const tRandom = randomTanah(tanah);
+  const wRandom = randomWaktu(200, 500);
+  tRandom.classList.add('muncul');
+  setTimeout(() => {
+    tRandom.classList.remove('muncul');
+    if (!selesai) munculkanTikus();
+  }, wRandom);
+}
+
+function mulai() {
+  selesai = false;
+  skor = 0;
+  papanSkor.textContent = 0;
+  munculkanTikus();
+  tikus.forEach(t => {
+    t.addEventListener('click', pukul)
+  });
+  setTimeout(() => {
+    selesai = true;
+  }, 10000);
+}
+
+function pukul() {
+  skor++;
+  this.parentNode.classList.remove('muncul');
+  bom.play();
+  papanSkor.textContent = skor;
+}
